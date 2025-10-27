2a4c429c9bad945b89d08a263a5c6dc28ad4432d
TheAlgorithm476 <thealgorithm476@gmail.com>
2025-10-27 15:47:21 +0100

    Setup C module

---
+++ CCoreAVR/main.swift
@@ -0,0 +1,8 @@
+//
+//  main.swift
+//  CCoreAVR
+//
+//  Created by Brent Van den Abbeel on 2025-10-27.
+//
+        
+
--- Package.swift
+++ Package.swift
@@ -1,23 +1,17 @@
-// swift-tools-version: 6.2
-// The swift-tools-version declares the minimum version of Swift required to build this package.
+// swift-tools-version: 5.7
 
 import PackageDescription
 
 let package = Package(
     name: "CCoreAVR",
     products: [
-        // Products define the executables and libraries a package produces, making them visible to other packages.
-        .library(
-            name: "CCoreAVR",
-            targets: ["CCoreAVR"]
-        ),
+        .library(name: "CCoreAVR", targets: ["CCoreAVR"])
     ],
     targets: [
-        // Targets are the basic building blocks of a package, defining a module or a test suite.
-        // Targets can depend on other targets in this package and products from dependencies.
         .target(
-            name: "CCoreAVR"
-        ),
-
+            name: "CCoreAVR",
+            path: "CCoreAVR",
+            sources: ["main.swift"]
+        )
     ]
 )
